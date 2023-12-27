2407711e95804d7e9d37d8606d25ec504a915582
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-12-27 20:21:40 -0300

    feat: configured product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,13 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
 export class Product {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ type: 'varchar', length: 70 })
   name: string;
 
+  @Column({ type: 'varchar', length: 355 })
   description: string;
 
+  @Column({ type: 'int' })
   price: number;
 
+  @Column({ type: 'int' })
   stock: number;
 
+  @Column({ type: 'varchar', length: 255 })
   image: string;
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 import { ProductsService } from './services/products.service';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [BrandsController, CategoriesController, ProductsController],
   providers: [BrandsService, CategoriesService, ProductsService],
 })
