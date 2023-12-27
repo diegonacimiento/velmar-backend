a207c9c01868ecb17b7bb4fe0989f48911cf9c5e
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-12-27 12:08:18 -0300

    feat: created all services

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
+import { BrandsService } from './services/brands.service';
+import { CategoriesService } from './services/categories.service';
+import { ProductsService } from './services/products.service';
 
 @Module({
   controllers: [BrandsController, CategoriesController, ProductsController],
+  providers: [BrandsService, CategoriesService, ProductsService],
 })
 export class ProductsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './controllers/orders.controller';
 import { UsersController } from './controllers/users.controller';
+import { OrdersService } from './services/orders.service';
+import { UsersService } from './services/users.service';
 
 @Module({
   controllers: [OrdersController, UsersController],
+  providers: [OrdersService, UsersService],
 })
 export class UsersModule {}
