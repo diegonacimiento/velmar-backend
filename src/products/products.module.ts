10850b5a26ec8fa81bb8852e754b07f5beea13da
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-12-27 12:04:44 -0300

    feat: created all controllers

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BrandsController } from './controllers/brands.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { ProductsController } from './controllers/products.controller';
 
-@Module({})
+@Module({
+  controllers: [BrandsController, CategoriesController, ProductsController],
+})
 export class ProductsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { OrdersController } from './controllers/orders.controller';
+import { UsersController } from './controllers/users.controller';
 
-@Module({})
+@Module({
+  controllers: [OrdersController, UsersController],
+})
 export class UsersModule {}
