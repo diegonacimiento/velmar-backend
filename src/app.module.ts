fba06f05b8e16357d9aa905667bb14a0258810c3
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-12-27 20:11:08 -0300

    feat: created and configured module database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { environment } from './environment';
@@ -13,6 +14,7 @@ import * as Joi from 'joi';
   imports: [
     ProductsModule,
     UsersModule,
+    DatabaseModule,
     ConfigModule.forRoot({
       envFilePath: environment[process.env.NODE_ENV] || '.env',
       isGlobal: true,
---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import config from 'src/config';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { database, user, password, host, port } = configService.postgres;
+        return {
+          type: 'postgres',
+          database,
+          username: user,
+          password,
+          host,
+          port,
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
